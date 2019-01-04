b390cd47a46087dc62b6728cb2decf80b957b58f
florencewambui <florencewambui.fw@gmail.com>
2019-01-04 16:19:41 +0300

    Exploring relationship between sentiment scores, nchars, number of words, number of unique words and usage of caps with toxicity level.

--- loading_data.R
+++ loading_data.R
@@ -5,6 +5,9 @@ library(tm)
 library(tidytext)
 library(wordcloud)
 library(stringr)
+library(gridExtra)
+library(ngram)
+
 
 train <- read_csv("train.csv")
 test = read_csv("test.csv")
@@ -162,6 +165,17 @@ train$comment_text[train$sentiment_score > 100]
 train$comment_text[train$sentiment_score < -500]                                       
 # the high positive and negative scores are mainly due to repetition of words and phrases
 # remove repeated words
+remove_repeated_words = function(x){
+  x = tolower(x)
+  x = gsub("'", "", x)
+  x = gsub("[[:punct:]]", " ", x)
+  x = gsub("\n", "", x)
+  x = gsub("\t", "", x)
+  x = gsub("\\s+", " ", x)
+  d = unlist(strsplit(x, " "))
+  x = paste(unique(d), collapse = " ")
+  
+}
 train$no_repeat_words_comments = as.vector(apply(X = train[,2, drop = F], MARGIN = 1, FUN = remove_repeated_words))
 #colnames(train)[9] = "no_repeat_words_comments"
 unnested_words_sentiments = train %>% 
@@ -170,23 +184,22 @@ unnested_words_sentiments = train %>%
 unnested_words_sentiments$sentiment  = if_else(unnested_words_sentiments$sentiment == "positive", 1, -1)
 
 
-train_sentiments = unnested_words_sentiments %>%
+train = unnested_words_sentiments %>%
   group_by(id) %>%
   summarise(sentiment_score = sum(sentiment)) %>%
   right_join(train, by = "id") # 22% comments have missing sentiment score
 
-summary(train_sentiments$sentiment_score) # now the sentiment are within a more reasonable range
+summary(train$sentiment_score) # now the sentiment are within a more reasonable range
 
-threat_comments = train_sentiments[train_sentiments$threat == 1,]
-severe_toxic_comments = train_sentiments[train_sentiments$severe_toxic == 1,]
-toxic_comments = train_sentiments[train_sentiments$toxic == 1,]
-obscene_comments = train_sentiments[train_sentiments$obscene == 1,]
-insult_comments = train_sentiments[train_sentiments$insult == 1,]
-identity_hate_comments = train_sentiments[train_sentiments$identity_hate == 1,]
+threat_comments = train[train$threat == 1,]
+severe_toxic_comments = train[train$severe_toxic == 1,]
+toxic_comments = train[train$toxic == 1,]
+obscene_comments = train[train$obscene == 1,]
+insult_comments = train[train$insult == 1,]
+identity_hate_comments = train[train$identity_hate == 1,]
+non_toxic_comments = train[train$toxicity_level == 0,]
 
-train_sentiments$toxicity_level = rowSums(train_sentiments[,4:9])
 
-non_toxic_comments = train_sentiments[train_sentiments$toxicity_level == 0,]
 
 summary(severe_toxic_comments$sentiment_score)
 summary(toxic_comments$sentiment_score)
@@ -199,6 +212,123 @@ summary(non_toxic_comments$sentiment_score)
 #Any comment with a toxic level is likely to have a negative sentiment analysis score
 
 
+sev_plot = ggplot(severe_toxic_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") + 
+  ggtitle("Severe Toxic Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+tox_plot = ggplot(toxic_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") + 
+  ggtitle("Toxic Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+obs_plot = ggplot(obscene_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") +
+  ggtitle("Obscene Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+ins_plot = ggplot(insult_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") +
+  ggtitle("Insult Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+threat_plot = ggplot(threat_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") +
+  ggtitle("Threat Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+ident_plot = ggplot(identity_hate_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") +
+  ggtitle("Identity Hate Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+nontox_plot = ggplot(non_toxic_comments, aes(x = sentiment_score)) + geom_histogram(fill = "red") + 
+  ggtitle("Non-toxic Sentiment Scores") + scale_x_continuous(breaks = seq(-60, 40, 10), labels = seq(-60, 40, 10))
+grid.arrange(sev_plot, tox_plot, obs_plot, ins_plot, threat_plot, ident_plot, nontox_plot)
+# from the plots, majority of toxic comments at various levels had negative sentiment score. Try combining various 
+# sentiment analysis methods to create more robust sentiment scores
+
+train$comments_nchar = nchar(train$comment_text)
+
+clean_comments = function(x){
+  x = tolower(x)
+  x = gsub("'", "", x)
+  x = gsub("[[:punct:]]", " ", x)
+  x = gsub("\n", "", x)
+  x = gsub("\t", "", x)
+  x = gsub("\\s+", " ", x)
+  
+}
+
+train$clean_comments = clean_comments(train$comment_text)
+train$number_words = sapply(strsplit(train$clean_comments, "\\s+"), length)
+train$number_unique_words = sapply(strsplit(train$no_repeat_words_comments, "\\s+"), length)
+train$prop_duplicated_words = 100 - (round((train$number_unique_words/train$number_words), 2) * 100)
 
 
-                                               
+threat_comments = train[train$threat == 1,]
+severe_toxic_comments = train[train$severe_toxic == 1,]
+toxic_comments = train[train$toxic == 1,]
+obscene_comments = train[train$obscene == 1,]
+insult_comments = train[train$insult == 1,]
+identity_hate_comments = train[train$identity_hate == 1,]
+non_toxic_comments = train[train$toxicity_level == 0,]
+
+# number_unique_words
+sev_plot = ggplot(severe_toxic_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Severe Toxic Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+tox_plot = ggplot(toxic_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Toxic Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+obs_plot = ggplot(obscene_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") +
+  ggtitle("Obscene Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+ins_plot = ggplot(insult_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") +
+  ggtitle("Insult Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+threat_plot = ggplot(threat_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") +
+  ggtitle("Threat Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+ident_plot = ggplot(identity_hate_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") +
+  ggtitle("Identity Hate Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+nontox_plot = ggplot(non_toxic_comments, aes(x = number_unique_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Non-toxic Number of Unique Words") + scale_x_continuous(breaks = seq(0, 600, 50), labels = seq(0, 600, 50))
+grid.arrange(sev_plot, tox_plot, obs_plot, ins_plot, threat_plot, ident_plot, nontox_plot)
+
+# number_words
+sev_plot = ggplot(severe_toxic_comments, aes(x = number_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Severe Toxic Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+tox_plot = ggplot(toxic_comments, aes(x = number_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Toxic Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+obs_plot = ggplot(obscene_comments, aes(x = number_words)) + geom_histogram(fill = "red") +
+  ggtitle("Obscene Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+ins_plot = ggplot(insult_comments, aes(x = number_words)) + geom_histogram(fill = "red") +
+  ggtitle("Insult Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+threat_plot = ggplot(threat_comments, aes(x = number_words)) + geom_histogram(fill = "red") +
+  ggtitle("Threat Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+ident_plot = ggplot(identity_hate_comments, aes(x = number_words)) + geom_histogram(fill = "red") +
+  ggtitle("Identity Hate Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+nontox_plot = ggplot(non_toxic_comments, aes(x = number_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Non-toxic Number of Words") + scale_x_continuous(breaks = seq(0, 1400, 200), labels = seq(0, 1400, 200))
+grid.arrange(sev_plot, tox_plot, obs_plot, ins_plot, threat_plot, ident_plot, nontox_plot)
+
+# proportion of duplicate words
+sev_plot = ggplot(severe_toxic_comments[severe_toxic_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Severe Toxic Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+tox_plot = ggplot(toxic_comments[toxic_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Toxic Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+obs_plot = ggplot(obscene_comments[obscene_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") +
+  ggtitle("Obscene Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+ins_plot = ggplot(insult_comments[insult_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") +
+  ggtitle("Insult Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+threat_plot = ggplot(threat_comments[threat_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") +
+  ggtitle("Threat Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+ident_plot = ggplot(identity_hate_comments[identity_hate_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") +
+  ggtitle("Identity Hate Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+nontox_plot = ggplot(non_toxic_comments[non_toxic_comments$prop_duplicated_words >= 0,], aes(x = prop_duplicated_words)) + geom_histogram(fill = "red") + 
+  ggtitle("Non-toxic Proportion of Duplicate Words") + scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10))
+grid.arrange(sev_plot, tox_plot, obs_plot, ins_plot, threat_plot, ident_plot, nontox_plot)
+
+# if a comment has more than 60% of duplicated words, then it likely falls under one category of toxic
+
+train$use_2_caps = grepl("[A-Z]{2,}",train$comment_text)
+train$use_3_caps = grepl("[A-Z]{3,}",train$comment_text)
+train$use_6_caps = grepl("[A-Z]{6,}",train$comment_text)
+
+round(prop.table(table(toxic_comments$use_3_caps)),2)
+round(prop.table(table(severe_toxic_comments$use_3_caps)), 2)
+round(prop.table(table(obscene_comments$use_3_caps)), 2)
+round(prop.table(table(insult_comments$use_3_caps)), 2)
+round(prop.table(table(threat_comments$use_3_caps)), 2)
+round(prop.table(table(identity_hate_comments$use_3_caps)),2)
+round(prop.table(table(non_toxic_comments$use_3_caps)),2)
+
+# proportion of non_toxic comments with at least 3 caps is lower than for toxic comments
+round(prop.table(table(toxic_comments$use_6_caps)),2)
+round(prop.table(table(severe_toxic_comments$use_6_caps)), 2)
+round(prop.table(table(obscene_comments$use_6_caps)), 2)
+round(prop.table(table(insult_comments$use_6_caps)), 2)
+round(prop.table(table(threat_comments$use_6_caps)), 2)
+round(prop.table(table(identity_hate_comments$use_6_caps)),2)
+round(prop.table(table(non_toxic_comments$use_6_caps)),2)
+# proportion of non_toxic comments with at least 6 caps is significantly lower than for toxic comments
+
+
